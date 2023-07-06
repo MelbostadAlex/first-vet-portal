e197673c5d2b21a88661cc6dd65350d65e5b5e9e
Alexander Nilsson Melbostad <alexander.nilssonmelbostad@gmail.com>
2023-07-06 16:39:48 +0200

    Added filtering per employee

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import { usePagination } from "react-use-pagination";
 import axios from 'axios';
 
 const ScheduleTable = () => {
     const [ schedules, setSchedules] = useState([]);
+
+    const {
+        currentPage,
+        totalPages,
+        setNextPage,
+        setPreviousPage,
+        nextEnabled,
+        previousEnabled,
+        startIndex,
+        endIndex,
+    } = usePagination({ totalItems: schedules.length, initialPage: 0, initialPageSize: 6});
 
     const getSchedules = () => {
         axios.get( "http://127.0.0.1:8000/api/schedule").then(res => {
@@ -10,6 +22,20 @@ const ScheduleTable = () => {
             setSchedules(data)
         })
     };
+
+    const filterOnEmployee = (value) => {
+        if(!value || value === "All") {
+            return setSchedules(schedules);
+        }
+
+       let filteredSchedules = schedules.filter(item => item.employeeName === value);
+
+       setSchedules(filteredSchedules)
+    }
+
+    const filterEmployeeNames = () => {
+        return [...new Set(schedules.map(item => item.employeeName))]
+    }
 
     useEffect(() => {
         getSchedules();
@@ -25,9 +51,10 @@ const ScheduleTable = () => {
                 <div className="my-2 flex sm:flex-row flex-col">
                     <div className="flex flex-row mb-1 sm:mb-0">
                         <div className="relative">
-                            <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                {schedules.map(schedule =>
-                                    <option> { schedule.employeeName }</option>
+                            <select onChange={({ target: { value } }) => filterOnEmployee(value)} className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                <option key="all"> All </option>
+                                {filterEmployeeNames().map((employee, index) =>
+                                    <option key={index}> { employee }</option>
                                 )}
                             </select>
                             <div
@@ -63,7 +90,7 @@ const ScheduleTable = () => {
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Pris
+                                    Pris (SEK)
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
@@ -76,8 +103,8 @@ const ScheduleTable = () => {
                             </tr>
                             </thead>
                             <tbody>
-                            {schedules.map(schedule =>
-                                <tr>
+                            {schedules.slice(startIndex, endIndex).map((schedule, index) =>
+                                <tr key={index}>
                                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                         <div className="flex items-center">
                                             <div className="flex-shrink-0 w-10 h-10">
@@ -95,11 +122,11 @@ const ScheduleTable = () => {
                                         </div>
                                     </td>
                                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-no-wrap">999 SEK</p>
+                                        <p className="text-gray-900 whitespace-no-wrap">{ schedule.price }</p>
                                     </td>
                                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                         <p className="text-gray-900 whitespace-no-wrap">
-                                            Jan 21, 2020
+                                            { schedule.time }
                                         </p>
                                     </td>
                                     <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -115,15 +142,13 @@ const ScheduleTable = () => {
                         <div
                             className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
                             <span className="text-xs xs:text-sm text-gray-900">
-                                Showing 1 to 4 of 50 Entries
+                                Showing {currentPage} of {totalPages} of {schedules.length} Entries
                             </span>
                             <div className="inline-flex mt-2 xs:mt-0">
-                                <button
-                                    className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                                <button onClick={setPreviousPage} disabled={!previousEnabled} className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
                                     Prev
                                 </button>
-                                <button
-                                    className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                                <button onClick={setNextPage}  disabled={!nextEnabled} className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
                                     Next
                                 </button>
                             </div>
