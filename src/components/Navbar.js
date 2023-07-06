4e7459a2ddd524b2c569ec9cf5a3f3bc9533b180
Alexander Nilsson Melbostad <alexander.nilssonmelbostad@gmail.com>
2023-07-06 14:58:05 +0200

    Templating finished

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import MAIN_LOGO from '../assets/main_logo.png'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-white to-[#2c5364] border-y-2'>
+
+            <div className='flex text-black '>
+                <img src={MAIN_LOGO} alt="Logo" className='w-[300px] p-5' />
+                <h1 className='p-5 text-2xl flex items-center font-bold'> Schedule Tool </h1>
+            </div>
+
+            {/* Menu */}
+
+            <ul className='flex p-2 text-black'>
+                <li className='py-2 px-4'> Schedule </li>
+                <li><button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> Sign In </button></li>
+                <li> <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'> Sign Up </button> </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/ScheduleTable.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const ScheduleTable = () => {
+    const [ schedules, setSchedules] = useState([]);
+
+    const getSchedules = () => {
+        axios.get( "http://127.0.0.1:8000/api/schedule").then(res => {
+            let data = res.data;
+            setSchedules(data)
+        })
+    };
+
+    useEffect(() => {
+        getSchedules();
+    }, []);
+
+    return (
+        <div className='h-[1000px] max-w-[1200px] mx-auto px-8 flex flex-col justify-center'>
+            <div className="container mx-auto px-4 sm:px-8">
+            <div className="py-8">
+                <div>
+                    <h2 className="text-2xl font-semibold leading-tight">Lediga tider</h2>
+                </div>
+                <div className="my-2 flex sm:flex-row flex-col">
+                    <div className="flex flex-row mb-1 sm:mb-0">
+                        <div className="relative">
+                            <select className="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                {schedules.map(schedule =>
+                                    <option> { schedule.employeeName }</option>
+                                )}
+                            </select>
+                            <div
+                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                </svg>
+                            </div>
+                        </div>
+                        <div className="relative">
+                            <div
+                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                    <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                        <table className="min-w-full leading-normal">
+                            <thead>
+                            <tr>
+                                <th
+                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Veterinär
+                                </th>
+                                <th
+                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Pris
+                                </th>
+                                <th
+                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Datum
+                                </th>
+                                <th
+                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Boka
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {schedules.map(schedule =>
+                                <tr>
+                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <div className="flex items-center">
+                                            <div className="flex-shrink-0 w-10 h-10">
+                                                { schedule.employeeName === "Jane Doe"
+                                                    ? <img className="w-full h-full rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" alt=""/>
+                                                    : <img className="w-full h-full rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" alt=""/>
+                                                }
+                                            </div>
+
+                                            <div className="ml-3">
+                                                <p className="text-gray-900 whitespace-no-wrap">
+                                                    { schedule.employeeName }
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <p className="text-gray-900 whitespace-no-wrap">999 SEK</p>
+                                    </td>
+                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <p className="text-gray-900 whitespace-no-wrap">
+                                            Jan 21, 2020
+                                        </p>
+                                    </td>
+                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                            <span
+                                                className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> Boka </button>
+                                            </span>
+                                    </td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                        <div
+                            className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                            <span className="text-xs xs:text-sm text-gray-900">
+                                Showing 1 to 4 of 50 Entries
+                            </span>
+                            <div className="inline-flex mt-2 xs:mt-0">
+                                <button
+                                    className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                                    Prev
+                                </button>
+                                <button
+                                    className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                                    Next
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default ScheduleTable
